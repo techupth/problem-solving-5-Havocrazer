6ed9939501e0450d81c3e86b33871fc57d675483
Havocrazer <92991759+Havocrazer@users.noreply.github.com>
2024-03-13 15:25:39 +0700

    feat: add finish assignment

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,39 @@
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i])
+// Return the running sum of nums.
+
+// Example 1:
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+// Example 2:
+// Input: nums = [1,1,1,1,1]
+// Output: [1,2,3,4,5]
+// Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+// Example 3:
+// Input: nums = [3,1,2,10,1]
+// Output: [3,4,6,16,17]
+
+// **Constraints:**
+
+// 1 <= nums.length <= 1000
+// -10^6 <= nums[i] <= 10^6
+
 function runningSum(nums) {
-    
-};
+  let sumNums = [];
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum = sum + nums[i];
+    sumNums.push(sum);
+  }
+  return sumNums;
+}
+
+const result1 = runningSum([1, 2, 3, 4]);
+const result2 = runningSum([1, 1, 1, 1, 1]);
+const result3 = runningSum([3, 1, 2, 10, 1]);
+
+console.log(result1); // [1, 3, 6, 10]
+console.log(result2); // [1,2,3,4,5]
+console.log(result3); // [3,4,6,16,17]
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,37 @@
+// Exercise 2: Move Zeros
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+// Note that you must do this in-place without making a copy of the array.
+
+// Example 1:
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+
+// Example 2:
+// Input: nums = [0]
+// Output: [0]
+
+// **Constraints:**
+
+// 1 <= nums.length <= 10**4
+// -2**31 <= nums[i] <= 2**31 - 1
+
 function moveZeroes(nums) {
-    
-};
+  let newNums = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      newNums.push(nums[i]);
+    }
+  }
+  while (newNums.length < nums.length) {
+    newNums.push(0);
+  }
+  return newNums;
+}
+
+const result1 = moveZeroes([0, 1, 0, 3, 12]);
+const result2 = moveZeroes([0]);
+const result3 = moveZeroes([3, 0, 2, 10, 1, 0, 5]);
+
+console.log(result1); // [1,3,12,0,0]
+console.log(result2); // [0]
+console.log(result3); // [3,2,10,1,5,0,0]
